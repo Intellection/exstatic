3350be82c3b6b7aa15bc6d73c1922d0378be2425
Brook <71849503+Primebrook@users.noreply.github.com>
2025-01-30 12:21:14 +0000

    allow distributions to define their own constructors

--- lib/exstatic/distribution.ex
+++ lib/exstatic/distribution.ex
@@ -1,9 +1,7 @@
 defmodule Exstatic.Distribution do
   @type t() :: struct()
   @type error() :: {:error, atom()}
-  @type result(t) :: {:ok, t} | error()
 
-  @callback new(mean :: float(), std_dev :: float()) :: result(t())
   @callback mean(distribution :: t()) :: float()
   @callback variance(distribution :: t()) :: float()
   @callback std_dev(distribution :: t()) :: float()
